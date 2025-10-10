b5ff32bcab12f2b4334390020dcb4d7b53155cbe
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-10 20:56:11 -0300

    chore: adding rules on service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // project settings that I like to use.
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        Model::shouldBeStrict(
+            app()->isLocal()
+        );
+
+        // https://laravel.com/docs/12.x/eloquent-relationships#automatic-eager-loading
+        Model::automaticallyEagerLoadRelationships();
     }
 
     /**
