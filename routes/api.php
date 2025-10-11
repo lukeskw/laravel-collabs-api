af728e6fb3144ffa10f9578462e53896a4045412
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-11 20:06:23 -0300

    feat: creating api routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,40 @@
 <?php
 
+use App\Api\v1\Http\Actions\Collaborators\ImportCollaboratorsAction;
 use App\Api\v1\Http\Controllers\AuthenticationController;
+use App\Api\v1\Http\Controllers\CollaboratorController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::post('login', [AuthenticationController::class, 'login']);
+Route::prefix('v1')
+    ->as('api.v1.')
+    ->group(function (): void {
+        Route::prefix('auth')
+            ->as('auth.')
+            ->group(function (): void {
+                Route::post('login', [AuthenticationController::class, 'login'])
+                    ->name('login');
+                Route::post('refresh', [AuthenticationController::class, 'refresh'])
+                    ->name('refresh');
+            });
 
-        Route::middleware('auth:api')->group(function () {
-            Route::get('me', [AuthenticationController::class, 'me']);
-            Route::post('logout', [AuthenticationController::class, 'logout']);
+        Route::middleware('auth:api')->group(function (): void {
+            Route::get('auth/me', [AuthenticationController::class, 'me'])
+                ->name('auth.me');
+            Route::post('auth/logout', [AuthenticationController::class, 'logout'])
+                ->name('auth.logout');
+
+            Route::prefix('collaborators')
+                ->as('collaborators.')
+                ->group(function (): void {
+                    Route::post('import', ImportCollaboratorsAction::class)
+                        ->name('import');
+                    Route::controller(CollaboratorController::class)->group(function (): void {
+                        Route::get('', 'index')->name('index');
+                        Route::post('', 'store')->name('store');
+                        Route::get('{collaborator}', 'show')->name('show');
+                        Route::match(['put', 'patch'], '{collaborator}', 'update')->name('update');
+                        Route::delete('{collaborator}', 'destroy')->name('destroy');
+                    });
+                });
         });
-
-        Route::post('refresh', [AuthenticationController::class, 'refresh']);
     });
-});
