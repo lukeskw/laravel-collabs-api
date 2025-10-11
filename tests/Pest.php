ca4e75f4ac74a208219624549c3cc222db00e711
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-11 20:07:41 -0300

    test: creating custom helper to create auth header on tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 uses(TestCase::class, RefreshDatabase::class)->in('Feature');
 uses(TestCase::class)->in('Unit');
+
+/**
+ * @return array<string, string>
+ */
+function apiHeaders(User $user): array
+{
+    $token = JWTAuth::fromUser($user);
+
+    return [
+        'Authorization' => sprintf('Bearer %s', $token),
+        'Accept' => 'application/json',
+    ];
+}
