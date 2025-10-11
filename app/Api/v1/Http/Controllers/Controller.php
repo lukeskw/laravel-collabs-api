e03f58cc1ccf97763951a92efed9e761c8514d16
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-11 20:03:40 -0300

    feat: creating base controller

--- app/Api/v1/Http/Controllers/Controller.php
+++ app/Api/v1/Http/Controllers/Controller.php
@@ -2,7 +2,12 @@
 
 namespace App\Api\v1\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests;
+    use ValidatesRequests;
 }
