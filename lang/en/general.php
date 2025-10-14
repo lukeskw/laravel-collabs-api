afd1eea29d77f8f04437ac5dd15ea30c483d40d7
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-14 20:55:59 -0300

    feat: adding new translated variables

--- lang/en/general.php
+++ lang/en/general.php
@@ -2,4 +2,5 @@
 
 return [
     'import_started' => 'Processing started successfully.',
+    'import_failed' => 'An error occurred during processing. Our team has been notified.',
 ];
--- lang/pt_BR/general.php
+++ lang/pt_BR/general.php
@@ -2,4 +2,5 @@
 
 return [
     'import_started' => 'Processamento iniciado com sucesso.',
+    'import_failed' => 'Ocorreu um erro durante o processamento. Nossa equipe já foi notificada.',
 ];
